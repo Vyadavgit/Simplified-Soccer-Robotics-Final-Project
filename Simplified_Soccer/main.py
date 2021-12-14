aa1441d08e8f33dd0c909ec25600d1fdda062054
Gabriel de Sa <contact@gabrieldesa.me>
2021-12-13 20:26:55 -0600

    Added the base system for ball following.

--- Simplified_Soccer/main.py
+++ Simplified_Soccer/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.nxtdevices import (LightSensor)
+                                    UltrasonicSensor, GyroSensor)
+from pybricks.iodevices import I2CDevice
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
@@ -10,6 +10,23 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 import time
 import random
 
+# Create InfraredSensor class
+class InfraredSensor():
+    def __init__(self, port):
+        self.sensor = I2CDevice(port, 0x01)
+    
+    def get_zone(self):
+        """Returns Zone that IR signal is observed in."""
+        return int.from_bytes(self.sensor.read(0x42, length=1), "little")
+    
+    def get_strength(self): 
+        """Returns an array with the relative strength of IR in each zone."""
+        retArray = []
+        for i in range(5):
+            strength = int.from_bytes(self.sensor.read(0x43+i, length=1), "little")
+            retArray.append(strength)
+        return retArray
+        
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
@@ -36,34 +53,66 @@ def rotate(right, left, angle=90, velocity = 150):
     right.run_target(velocity, desiredAngle, Stop.HOLD, False)
     left.run_target(velocity, -desiredAngle)
 
-def findBall(sonicSens, right, left):
-    # """Find the object by moving the vehicle in multiple directions"""
-    # for i in range(18):
-    #     rotate(right, left, 20) # 20 degree increments
-    #     dist = sonicSens.distance() / 1000
-    #     if dist < .45:
-    #         # turn to the direction of opponent's base
-    #         # keep pushing the ball forward until you hit the opponent's base line color
-    #         # bounce back
+def score(gyroSensor, right, left):
+    ev3.screen.print("scoring")
+    while(gyroSensor.angle() < -5 or gyroSensor.angle() > 5):
+        if(gyroSensor.angle() < 0):
+            # if its less than 0 turn right
+            right.run(-160)
+            left.run(160)
+        else:
+            # if its more than 0 turn left
+            right.run(160)
+            left.run(-160)
+    else:
+        goToTarget(right, left, .3, 500)
 
-def wander(colorS, lColorS, sonicS, right, left):
-    # boolean = True
-    # direction = [0, 90, -90, 180]
-    # while(boolean):
-    #     rotate(right, left, direction[random.randint(0, 2)])
-    #     right.run(360), left.run(360)
-    #     while()
+
+def align(irSensor, right, left):
+    ev3.screen.print("aligning")
+    zones = [4, 5, 6]
+    while irSensor.get_zone() not in zones: 
+        if(irSensor.get_zone() < 5):
+            # if its less than 5 its on its left 
+            right.run(160)
+            left.run(-160)
+        else: 
+            # if its more than 5 its on the right
+            right.run(-160)
+            left.run(160)
+    right.stop()
+    left.stop()
+    return True
+
+def ballFollow(irSensor, right, left):
+    ev3.screen.print("ballFollow")
+    if align(irSensor, right, left):
+        while irSensor.get_strength()[2] < 40 or irSensor.get_strength()[1] < 40: 
+            right.run(160)
+            left.run(160)
+        else:
+            right.stop()
+            left.stop()
+            return "score"
 
 
 # Create your objects here.
 def main():
     rightMotor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
     leftMotor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
-    colorSensor = ColorSensor(Port.S4)
-    leftColorSensor = ColorSensor(Port.S2)
-    sonicSensor = UltrasonicSensor(Port.S1)
+    colorSensor = ColorSensor(Port.S3)
+    leftColorSensor = ColorSensor(Port.S4)
+    gyroSensor = GyroSensor(Port.S1)
+    irSensor = InfraredSensor(Port.S2)
+    # direction info 0x42 
+    # 0x43 strength at 5 ir segments
+    # read separately 
     # Write your program here.
     ev3.speaker.beep()
-    wander(colorSensor, leftColorSensor, sonicSensor, rightMotor, leftMotor) # wander to search the ball
+    # if ballFollow(irSensor, rightMotor, leftMotor) == "score":
+    #     score(gyroSensor, rightMotor, leftMotor)
+    while True: 
+        print(irSensor.get_strength())
+        time.sleep(1)
 main()
 
